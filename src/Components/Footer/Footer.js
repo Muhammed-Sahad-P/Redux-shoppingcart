f3b5115338195ea8d52ddd3f7c6764d577b0b419
Muhammed Sahad P <zanusahad@gmail.com>
2024-07-22 13:26:51 +0530

    added footer

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,9 +1,27 @@
-import React from 'react'
+import React from "react";
+import { FaInstagram, FaFacebook} from "react-icons/fa";
+import { FaXTwitter } from "react-icons/fa6";
+import "./Footer.css"
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <>
+    <footer className="footer">
+    <div className="footer-container">
+      <div className="footer-logo">
+        <p className="footer-logo-text">Shopping Cart</p>
+      </div>
+      <p className="footer-contact">xyz,abc | +91 1234567890 | shoppingcart@gmail.com</p>
+      <div className="footer-social">
+        <FaInstagram className="footer-social-icon instagram" />
+        <FaFacebook className="footer-social-icon facebook" />
+        <FaXTwitter  className="footer-social-icon twitter" />
+      </div>
+      <p className="footer-copy">&copy; 2024 Shopping Cart</p>
+    </div>
+  </footer>
+  </>
+  );
+};
 
-export default Footer
+export default Footer;
