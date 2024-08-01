d73c27e5a1123034e82a8b498c74c1b104c0cf6d
Muhammed Sahad P <zanusahad@gmail.com>
2024-08-01 18:29:31 +0530

    fix cart logo icon

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,26 +1,34 @@
-
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 import { LiaShoppingBagSolid } from "react-icons/lia";
-import './Navbar.css'; 
-import { useSelector } from 'react-redux';
+import "./Navbar.css";
+import { useSelector } from "react-redux";
 const Navbar = () => {
-const cartProducts = useSelector((state) => state.cart);
+  const cartProducts = useSelector((state) => state.cart);
 
   return (
     <nav className="navbar">
       <div className="navbar-container">
-        <Link to="/" className="navbar-logo">Shopping Cart</Link>
+        <Link to="/" className="navbar-logo">
+          Shopping Cart
+        </Link>
         <ul className="navbar-menu">
           <li className="navbar-item">
-            <Link to="/" className="navbar-link">Home</Link>
+            <Link to="/" className="navbar-link">
+              Home
+            </Link>
           </li>
           <li className="navbar-item">
-            <Link to="/product" className="navbar-link">Products</Link>
+            <Link to="/product" className="navbar-link">
+              Products
+            </Link>
           </li>
-          <Link to="/cart"  className="navbar-item">
-          <LiaShoppingBagSolid className="navbar-link" />
-          {cartProducts.length}
-          </Link>
+
+          <div className="navbar-item navbarcart">
+            <Link to="/cart">
+              <LiaShoppingBagSolid size={25} className="navbar-link" />
+            </Link>
+            <div className="length">{cartProducts.length}</div>
+          </div>
         </ul>
       </div>
     </nav>
